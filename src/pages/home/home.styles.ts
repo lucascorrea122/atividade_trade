ef9d91067a811bad5a6a604a5d58ee121b11de96
LucasCorrea <eslucascorrea@gmail.com>
2024-03-04 18:22:05 -0300

    Add files via upload

---
+++ src/pages/home/home.styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+// import BannerHeaderDark from '../home/bg-desktop-dark.jpg';
+// import BannerHeaderLight from "../../../assets/bg-desktop-light.jpg";
+
+export const HeaderBanner = styled.div`
+  /* background-image: url('bg-desktop-dark.jpg'); */
+  width: 100%;
+  height: 300px;
+  position: absolute;
+  top: 0px;
+  z-index: -1;  
+`;
+
+export const Section = styled.div`
+  width: 100%;
+  position: relative;
+  height: 100%;
+  justify-content: center;
+  display: flex;
+`;
+
+export const Container = styled.div`
+  height: 100%;
+  width: 450px;
+  margin-top: 50px;
+  padding: 0 20px 0 20px;
+`;
+
+export const Button = styled.button`
+  // widht: 50px;
+`;
